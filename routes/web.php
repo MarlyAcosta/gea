01dcd295a993e94fa1c47a3ade8db8c2dc2e54bd
JuanRobles2164 <juanestebanrobles3103@gmail.com>
2020-10-22 12:27:36 -0500

    Scaffold auth
    Se hace Scaffolding para la autenticación de usuarios (Registro, autenticación, recuperación de contraseñas, etc...)

--- routes/web.php
+++ routes/web.php
@@ -15,3 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', 'CategoryController@index')->name('index');
 
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
